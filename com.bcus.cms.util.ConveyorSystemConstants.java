a9a5246d18ed30514890718bd7d3b19f9fd5c1c4
hima-shah <hima4ks@gmail.com>
2015-04-27 09:51:19 +0530

    Create com.bcus.cms.util.ConveyorSystemConstants.java

---
+++ com.bcus.cms.util.ConveyorSystemConstants.java
@@ -0,0 +1,23 @@
+package com.bcus.cms.util;
+
+/**
+ * Application constants
+ * @author hima_shah
+ *
+ */
+
+public class ConveyorSystemConstants {
+	public static final int NOT_CONNECTED = -1;
+	public static final String WHITESPACE_SEPERATOR = " ";
+	public static final String COLON_SEPERATOR =":";
+	public static final String ARRIVAL_NODE = "ARRIVAL";
+	public static final String BAGGAGE_CLAIM_NODE = "BaggageClaim";
+	public static final String TICKETING_NODE = "Concourse_A_Ticketing";
+	public static final String FILE_PATH = "c:\\BCUS\\Input.txt";
+	public static final String OUTPUT_FILE_PATH = "c:\\BCUS\\Output.txt";
+	
+	public final static String SECTION ="# Section:";
+	public final static String CONVEYOR_SECTION = " Conveyor System";
+	public final static String FLIGHT_SECTION = " Departures";
+	public final static String BAGS_SECTION = " Bags";
+}
